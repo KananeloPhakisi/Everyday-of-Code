1cc23057e7d429ccef512b5075f46f443bb17d28
KananeloPhakisi <phakisikananelo0@gmail.com>
2022-08-06 23:50:00 +0200

    set up the project

---
+++ Stress Journal/stressJournalUi/assets/colors/colors.js
@@ -0,0 +1,18 @@
+const colors = {
+	background_blue: '#6DBBE4',
+	buttons: '#0077B6',
+	input_white: '#F5FCFE',
+	input_blue: '#CAF0F8',
+	date: '#3D3C3C',
+	journal_text: '#5C5050',
+	journal_text_container: '#CAF0F8',
+	very_happy: '#059B14',
+	happy: '#66E37A',
+	okay: '#E5D436',
+	sad: '#D45D5D',
+	very_sad: '#BF1A1A',
+	search_icon: '#C9A1A1',
+	check: '#66BA32',
+};
+
+export default colors;
