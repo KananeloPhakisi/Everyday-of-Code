c60078e37e6f0a08c2918482a5683ad8966cbf06
KananeloPhakisi <phakisikananelo0@gmail.com>
2022-10-03 18:57:54 +0200

    fix miniMaxSum

--- miniMaxSum.js
+++ miniMaxSum.js
@@ -1,24 +1,9 @@
 function miniMaxSum(arr) {
-    // Write your code here
     let maxVal = Math.max(...arr),
         minVal = Math.min(...arr);
-    let minSum = 0, maxSum = 0;
+    const sum = arr.reduce((a, b) => a+b, 0);
     
-    if (maxVal !== minVal) {
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] !== maxVal) minSum += arr[i];
-        }
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] !== minVal) maxSum += arr[i];
-        }
-    } else {
-        for (let i = 0; i < arr.length; i++) {
-            minSum += arr[i];
-        }
-        maxSum = minSum;
-    }
-    
-    console.log(minSum +" "+ maxSum);
+    console.log(Number(sum - maxVal) +" "+ Number(sum - minVal));
 }
 
 miniMaxSum([5, 5, 5, 5]);
