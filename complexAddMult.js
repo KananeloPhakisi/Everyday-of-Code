cd79d1c7122be63bb5d423256fe8a768ecc17842
KananeloPhakisi <phakisikananelo0@gmail.com>
2022-10-06 22:44:57 +0200

    add complexAddMult

---
+++ complexAddMult.js
@@ -0,0 +1,31 @@
+const complexSum = (complexNum1, complexNum2) => {
+    return {
+        real: complexNum1.real + complexNum2.real,
+        imag: complexNum1.imag + complexNum2.imag
+    }
+}
+
+const complexMult = (complexNum1, complexNum2) => {
+    return {
+        real: complexNum1.real * complexNum2.real - complexNum1.imag * complexNum2.imag,
+        imag: complexNum1.real * complexNum2.imag + complexNum1.imag * complexNum2.real
+    }
+}
+
+function main() {
+    const complexNum1 = {
+        real: 3,
+        imag: -1
+    }
+    const complexNum2 = {
+        real: 1,
+        imag: 4
+    }
+
+    console.log("Complex Sum: ");
+    console.log(complexSum(complexNum1, complexNum2));
+    console.log("Complex Multiplication: ");
+    console.log(complexMult(complexNum1, complexNum2));
+}
+
+main();
